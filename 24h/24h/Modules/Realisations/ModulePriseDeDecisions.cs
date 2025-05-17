91f7aaaa08598e21d417d8f9a0ee32ac4acb116f
twxlze <lisa.meunier71500@gmail.com>
2025-05-17 17:09:07 +0200

    Fix

--- 24h/24h/Modules/Realisations/ModulePriseDeDecisions.cs
+++ 24h/24h/Modules/Realisations/ModulePriseDeDecisions.cs
@@ -1,4 +1,5 @@
-﻿    using System;
+﻿
+    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,62 +44,7 @@ namespace IACryptOfTheNecroDancer.Modules.Realisations
         /// <returns>Le message à envoyer au serveur</returns>
         public string DeterminerNouvelleAction(string messageRecuDuServeur)
         {
-            string reponse = "SORTIR";
-            if (!this.IA.ModuleMemoire.HasCarte()) reponse = "CARTE";
-            else
-            {
-                if (this.IA.ModuleMemoire.Diamants.Count > 0 && this.mouvements.Count == 0)
-                {
-                    AlgorithmeCalculDistance parcours = new ParcoursLargeur(this.IA.ModuleMemoire.Carte); // On crée un algorithme de parcours en largeur à partir de la carte du module mémoire.
-                    Case depart = this.IA.ModuleMemoire.Carte.GetCaseAt(this.IA.ModuleMemoire.Joueur.Coordonnees); // On demande à la carte la case de "départ" (où se trouve le joueur)
-                    parcours.CalculerDistancesDepuis(depart); // On lance les calculs du parcours en largeur à partir de cette case.
-                    int distanceMin = -1;
-                    Objet diamantLePlusProche = null;
-
-                    foreach (Objet o in this.IA.ModuleMemoire.Diamants) // Pour tout les objets de la liste des diamants
-                    {
-                        // Calculer la distance jusqu'au diamant
-                        int distance = parcours.GetDistance(o.Position); // On calcule la distance jusqu'au diamant
-
-                        if (distanceMin == -1 || distance < distanceMin) // Si "distanceMin" vaut -1 ou si la distance jusqu 'à "distance" est plus petite que "distanceMin"
-                        {
-                            distanceMin = distance;
-                            diamantLePlusProche = o;
-                        }
-                    }
-                    this.mouvements = parcours.GetChemin(diamantLePlusProche.Position);
-                    
-                }
-
-
-                //
-                else if (this.mouvements.Count == 0)
-                {
-                    AlgorithmeCalculDistance parcours = new ParcoursLargeur(this.IA.ModuleMemoire.Carte);
-                    Case depart = this.IA.ModuleMemoire.Carte.GetCaseAt(this.IA.ModuleMemoire.Joueur.Coordonnees);
-                    Case sortie = this.IA.ModuleMemoire.Carte.GetCaseAt(
-                    this.IA.ModuleMemoire.Carte.CoordonneesSortie);
-                    parcours.CalculerDistancesDepuis(depart);
-                    this.mouvements = parcours.GetChemin(sortie);
-                }
-
-                if (this.mouvements.Count > 0)
-                {
-                    switch (this.mouvements[0])
-                    {
-                        case TypeMouvement.HAUT: reponse = "BOUGER|HAUT"; break;
-                        case TypeMouvement.GAUCHE: reponse = "BOUGER|GAUCHE"; break;
-                        case TypeMouvement.DROITE: reponse = "BOUGER|DROITE"; break;
-                        case TypeMouvement.BAS: reponse = "BOUGER|BAS"; break;
-                    }
-                    this.mouvements.RemoveAt(0);
-                }
-            }
-            if (reponse == "SORTIR")
-            {
-                this.IA.ArreterLaCommunication();
-            }
-            return reponse;
+            return "POOCHY";
         }
     #endregion
 }
--- 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
+++ 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("24h")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b6180967016a683e0fd248fe801c6ab5963664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f337f0429dfe8208fb8cd142682c4081919b648")]
 [assembly: System.Reflection.AssemblyProductAttribute("24h")]
 [assembly: System.Reflection.AssemblyTitleAttribute("24h")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
