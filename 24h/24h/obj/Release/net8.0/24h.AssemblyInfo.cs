7ce3b5f9b5849d6c006a5b9805ce3e61b2d77dc8
twxlze <lisa.meunier71500@gmail.com>
2025-05-17 14:51:46 +0200

    :!iuljly

--- 24h/24h/obj/Release/net8.0/24h.AssemblyInfo.cs
+++ 24h/24h/obj/Release/net8.0/24h.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("24h")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683041601bcb951ce6cab3628af3c05d7c21c6c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("24h")]
