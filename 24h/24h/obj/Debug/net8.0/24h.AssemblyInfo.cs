44c8160eddcf3f6f7176f1327cae60f147e683c6
BellegueulleMarie <lixy21200@gmail.com>
2025-05-17 15:52:30 +0200

    Personnages

--- 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
+++ 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("24h")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b6180967016a683e0fd248fe801c6ab5963664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c3e5f7e81a224fe7dfaca80a2f2fd8bf516436")]
 [assembly: System.Reflection.AssemblyProductAttribute("24h")]
 [assembly: System.Reflection.AssemblyTitleAttribute("24h")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
