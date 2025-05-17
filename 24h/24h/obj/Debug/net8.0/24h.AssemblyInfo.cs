78b6180967016a683e0fd248fe801c6ab5963664
BellegueulleMarie <lixy21200@gmail.com>
2025-05-17 15:05:16 +0200

    qey"(u-'i

--- 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
+++ 24h/24h/obj/Debug/net8.0/24h.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("24h")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4533759fd38ba49dd8f34cfc8e3e60e13dcf953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed6773b6c9404701154bdc3aae3c79a51f53497")]
 [assembly: System.Reflection.AssemblyProductAttribute("24h")]
 [assembly: System.Reflection.AssemblyTitleAttribute("24h")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
